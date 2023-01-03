4ef9a75d3d1c20c8f686e8cd7142337c5eff20da
Julesswiss <jerhivwor@gmail.com>
2023-01-03 13:57:01 -0800

    Test driven dev

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,43 +1,28 @@
 #!/usr/bin/python3
-"""This module prints a text with 2 new lines after each of these
-characters: ., ? and :"""
-import doctest
+"""Defines a text-indentation function."""
 
 
 def text_indentation(text):
-    """This module prints a text with 2 new lines after each of these
-    characters: ., ? and :
+    """Print text with two new lines after each '.', '?', and ':'.
     Args:
-        text (str): string to print
+        text (string): The text to print.
     Raises:
-        TypeError: test must be a string
+        TypeError: If text is not a string.
     """
-    if (type(text) is not str):
+    if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    i = 0
-    for _ in range(len(text)):
-        if (i <= len(text) - 1):
-            if(i == 0):
-                while (text[i] == " "):
-                    i += 1
-                    if i == len(text):
-                        break
-            print(text[i], end="")
-            if (text[i] == "." or text[i] == "?" or text[i] == ":"):
-                print()
-                print()
-                if (i < len(text) - 1 and text[i + 1] == " "):
-                    i += 1
-                    while (text[i] == " "):
-                        i += 1
-                        if i == len(text):
-                            break
-                else:
-                    i += 1
-            else:
-                i += 1
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
 
-
-if __name__ == '__main__':
-    doctest.testfile("./tests/5-text_indentation.txt")
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
