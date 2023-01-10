0ff6c3d46cbfd928e47faaef64e8f6bff963e619
Julesswiss <jerhivwor@gmail.com>
2023-01-10 09:40:19 -0800

    corrections

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,5 +1,11 @@
-#!/usr/bin/python3
-"""script that reads stdin line by line and computes metrics"""
+#!/usr/bin/python3ads from standard input and computes metrics.
+
+"""
+After every ten lines or the input of a keyboard interruption (CTRL + C),
+prints the following statistics:
+    - Total file size up to that point.
+    - Count of read status codes up to that point.
+"""
 
 
 def print_stats(size, status_codes):
@@ -11,7 +17,6 @@ def print_stats(size, status_codes):
     print("File size: {}".format(size))
     for key in sorted(status_codes):
         print("{}: {}".format(key, status_codes[key]))
-
 
 if __name__ == "__main__":
     import sys
